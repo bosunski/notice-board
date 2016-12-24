37d47edbe353245fc0f64a7fcda8cdec944edfde
bosunski <bosunski@gmail.com>
2016-12-24 18:38:39 +0100

    Delete noticeboard.conf.php

--- noticeboard.conf.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	define('DB', 'hackfunnab');
-	define('DBHOST', 'localhost');
-	define('DBUNAME', 'root');
-	define('DBPASS', 'gabriel10');
-	define('ABSPATH', dirname(__FILE__));
-	define('HOME', 'http://localhost/f');
-	define('AUTH_KEY', 'PAPAsukataLebanonx5x5');
-?>
